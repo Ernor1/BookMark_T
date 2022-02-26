88f39ea06bad93df7763a64702df28e62d9cb04a
Rukundo Honore <honorerukundo74@gmail.com>
2022-02-26 14:51:08 +0200

    Tailwind_devop

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: ["./public/index.html"],
+
+  theme: {
+    extend: {
+      colors: {
+        "bookmark-purple":"#be2edd",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
